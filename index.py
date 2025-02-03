b6fbaa3c8806ef0e5a190df8dac916e93225603d
abbey-glitch <abiodunonaolapi@gmail.com>
2025-02-03 18:06:18 +0100

    content loaded

--- index.py
+++ index.py
@@ -1,8 +1,23 @@
 import requests
+import pymysql
 from bs4 import BeautifulSoup
-feed = requests.get("https://www.cyclobold.com/")
-htmlData = feed.content
-feedback = BeautifulSoup(htmlData, "html.parser")
-all_img = feedback.find_all("img")
-print(all_img)
+
+
+db = pymysql.connect(
+    host = "localhost",
+    user = "root",
+    password = "password"
+    # database = "scrapedb"
+)
+
+cursor = db.cursor()
+cursor.execute("create database `scrapedb`")
+cursor.close()
+db.close()
+
+# feed = requests.get("https://www.cyclobold.com/")
+# htmlData = feed.content
+# feedback = BeautifulSoup(htmlData, "html.parser")
+# all_img = feedback.find_all("img")
+# print(all_img)
 # print(feedback.title.string)
